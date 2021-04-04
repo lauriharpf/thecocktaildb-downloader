78362ed2b688ccaf6c4d02c0b278335d3d4463fc
Lauri Harpf <lauri.harpf@acelvia.com>
2021-04-04 19:03:58 +0300

    - Add progress bar - Don't fail entire run if a single drink doesn't match the typings; just skip that drink & log it

--- src/CocktailAPI/fetchDrinks.ts
+++ src/CocktailAPI/fetchDrinks.ts
@@ -1,3 +1,4 @@
+import ProgressBar from "progress";
 import fetch from "node-fetch";
 import {
   Drink,
@@ -7,22 +8,34 @@ import {
 
 export const fetchDrinks = async (): Promise<Drink[]> => {
   const firstId = 11000;
-  const howMany = 2;
+  const howMany = 8000;
+  const bar = new ProgressBar(
+    "Fetching :bar :current/:total (:percent) ETA: :eta",
+    howMany
+  );
 
   const drinks: Drink[] = [];
   for (let id = firstId; id < firstId + howMany; id++) {
     const drink = await fetchDrink(id);
-    drinks.push(drink);
+    if (drink) {
+      drinks.push(drink);
+    }
+    bar.tick();
   }
 
   return drinks;
 };
 
-const fetchDrink = async (id: number): Promise<Drink> => {
+const fetchDrink = async (id: number): Promise<Drink | undefined> => {
   const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1";
   const result = await fetch(`${apiUrl}/lookup.php?i=${id}`);
-  const lookupResponse: LookupResponse = lookupResponseSchema.parse(
-    await result.json()
-  );
-  return lookupResponse.drinks[0];
+  try {
+    const lookupResponse: LookupResponse = lookupResponseSchema.parse(
+      await result.json()
+    );
+    return lookupResponse.drinks ? lookupResponse.drinks[0] : undefined;
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
 };
